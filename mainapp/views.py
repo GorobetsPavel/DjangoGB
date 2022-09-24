48b0ad795db347b09929d18ca5006d4712e80c84
Pavel <gorobecp@gmail.com>
2022-09-24 14:21:10 +0500

    lesson-2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def hello_view(request):
-    return HttpResponse('Hello world')
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
